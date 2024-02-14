a7e21bb48e76d6932a2e25bf89af511521a8b554
sandeep reddy <praveenpk7702@gmail.com>
2024-02-14 11:30:23 +0530

    code changed as subtraction and value of n2 changed

--- sum.java
+++ sum.java
@@ -1,9 +1,9 @@
-public class SumOfNumbers1  
+public class Sum #sandeep 
 {  
 public static void main(String args[])   
 {  
-int n1 = 225, n2 = 115, sum;  
-sum = n1 + n2;  
+int n1 = 225, n2 = 100, sum; #sandeep 
+sum = n1 - n2; #sandeep  
 System.out.println("The sum of numbers is: "+sum);  
 }  
 }  
